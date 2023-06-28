4b58b25e17cc77225257d53051986cdff698add7
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-28 22:56:24 +0100

    in the override method we use the parent method with SUPER

---
+++ src/main/java/OOP/AbstractClassThree/Animal.java
@@ -0,0 +1,28 @@
+package OOP.AbstractClassThree;
+
+
+public abstract class Animal {
+    //can hold a commund method
+     void eat(){
+         System.out.println("Animal");
+     }
+
+    /*
+     */
+     */
+}
+
+class Dog extends Animal {
+
+
+    public void eat() {
+
+        // If we want to access to the parent method
+        // we use super
+        super.eat();
+        System.out.println("Dog");
+
+
+    }
+
+}
