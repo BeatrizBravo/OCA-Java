e211bc177542a8ad312f7ffba8a7b17288e316fb
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-06-28 22:38:23 +0100

    created a simple public class animal that has a child dog. Dog is inherited from Animal

---
+++ src/main/java/OOP/Animal.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class Animal {
+
+}
+
+class Dog extends Animal{
+    // the class cannot inherited from multiple classes
+
+
+    // classes can be only public or like in like 7
+    // no private, protected
+}
